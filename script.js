95b28059cf8e4a20a325a6808c75d1c61ee894d1
Dayan <m.dayan5689@gmail.com>
2022-10-23 18:59:33 -0700

    Created Gameboard and function to create players

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const Players = (name, icon) => {
-  const play = (arr) => {
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] == "d") {
-        arr[i] = icon;
+  return { name, icon };
+};
+
+const CreatePlayer = () => {
+  const name = prompt("Enter player's name: ");
+  return Players(name, "X");
+};
+
+const GameBoard = (() => {
+  const game = ["d", "d", "d", "d", "d", "d", "d", "d", "d"];
+  const play = (icon) => {
+    let index = Math.floor(Math.random() * 9);
+    while (game.indexOf("d") != -1) {
+      if (game[index] != "d") {
+        game[index] = icon;
+      } else {
+        index = Math.floor(Math.random() * 9);
       }
+      return game;
     }
   };
-
-  return { name, icon, play };
-};
+  return { play };
+})();
 
